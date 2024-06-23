46f0dba4ab8d1a9ce2d7198eec8285994e8085db
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-06-23 16:27:37 +0300

    starting to navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom"; //Routes kapsayıcı , içinde route ile yönlendirme yapacağız
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* home elementine local host 3000 de ulaştık */}
+        <Route path="/AboutUs" element={<AboutUs />} />
+        {/* AboutUs elementine local host 3000/AboutUs ta ulaştık */}
+      </Routes>
     </div>
   );
 }

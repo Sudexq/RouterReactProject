ae00abc260d3e0ab6e8d15d0b291b723b0815048
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-06-23 16:38:43 +0300

    nav update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
+    <nav>
       <Link to="/">Home</Link>
       <Link to="/aboutUs">About</Link>
-    </div>
+    </nav>
   );
 }
 

469c52b4bff52331fa74f69622538155a64ff371
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-06-28 22:48:00 +0300

    useParams
    adding useParams to MemberDetail.js

---
+++ src/components/MemberDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function MemberDetail() {
+  const { memberId } = useParams();
+
+  return <div>MemberDetail {memberId}</div>;
+}
+
+export default MemberDetail;
